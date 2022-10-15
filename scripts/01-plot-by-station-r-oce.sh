21f99ce8f7736aad27e06f3df3ad71058becae7a
Jesse McNichol <5956604+jcmcnch@users.noreply.github.com>
2022-10-14 19:13:21 -0700

    Update repo

--- scripts/01-plot-by-station-r-oce.sh
+++ scripts/01-plot-by-station-r-oce.sh
@@ -1,12 +1,31 @@
 #!/bin/bash -i
 conda activate r-oce
 
-for station in `tail -n+2 input/P16S_metadata.tsv  | cut -f1 | cut -f1-2 -d- | sort | uniq`; do
+mkdir -p CTD-plots
+
+infile=input/P16S_metadata.tsv
+
+for station in `tail -n+2 $infile | cut -f1 | cut -f1-2 -d- | sort | uniq`; do
        
-	stationNo=`echo $station | cut -f2 -d-`
+	stationNo=`echo $station | cut -f2 -d- | sed 's/S//'`
 	paddedStationNo=`printf  "%05d\n" $stationNo`
-	echo $paddedStationNo
-	#./scripts/01-make-plots.R P16S/33RR200501_00111_00002_ct1.stripped.csv 500 "This is a title" outfile.pdf
-	echo $station
+	maxDepth=`grep $station $infile | cut -f9 | sort -gr | head -n1`
+
+	latTSV=`grep $station $infile | cut -f7 | sort | uniq`
+	lonTSV=`grep $station $infile | cut -f6 | sort | uniq`
+
+	#add loop to account for multiple CTD casts per station
+	for inputFile in `ls P16S/33RR200501_$paddedStationNo*stripped.csv`; do
+
+		filestem=`basename $inputFile .stripped.csv`
+		dirname=`dirname $inputFile`
+	        latCSV=`grep "LATITUDE =" $dirname/$filestem.csv | cut -f2 -d\= | sed 's/ //g'`
+        	lonCSV=`grep "LONGITUDE =" $dirname/$filestem.csv | cut -f2 -d\= | sed 's/ //g'`
+	
+		outfile=CTD-plots/$station.$filestem.CTDprofile.pdf
+
+		./scripts/01-make-plots.R $inputFile $maxDepth "CTD Profile for $station (ASVlatlong=$latTSV,$lonTSV; CTDlatlong=$latTSV,$lonTSV)" $outfile
+	
+	done 
 
 done
